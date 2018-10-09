5f5d6bd300ba6fbd8babe815d67937a78cb587f6
unknown <TongLT@ALS-Dev.alcvietnam.com>
2018-10-09 15:43:15 +0700

    Initial commit

---
+++ app/src/main/java/thelam365/bai_dau_tien/MainActivity.java
@@ -0,0 +1,14 @@
+package thelam365.bai_dau_tien;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
